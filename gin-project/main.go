32ab62cfd9aa4ee3d926d3d4ecfdfcba8d0bdbb3
jiewenk <jiewenk1996@163.com>
2023-08-01 23:11:45 +0800

    template render

--- gin-project/main.go
+++ gin-project/main.go
@@ -3,12 +3,19 @@ package main
 import "github.com/gin-gonic/gin"
 
 func Index(ctx *gin.Context) {
-	ctx.HTML(200, "index.html", nil)
+	ctx.HTML(200, "index/index.html", "这是Index界面")
+}
+
+func User(ctx *gin.Context) {
+	ctx.HTML(200, "user/user.html", gin.H{
+		"content": "这是User界面,Content",
+	})
 }
 
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("template/*")
+	router.LoadHTMLGlob("template/**/*")
 	router.GET("/", Index)
+	router.GET("/user", User)
 	router.Run(":9999")
 }
